93f6838eb2ae1fa6d5a0ef3903792721451de1fd
TechTitan8 <sarangoutham1@gmail.com>
2025-06-14 10:11:48 -0400

    Fix Vercel build configuration

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py collectstatic --noinput
+# Install Python dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
